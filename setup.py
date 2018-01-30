9e3697b5bd3ccb58f2193432eac3f1f1ef789d3c
Autophagy <mika@crate.io>
2018-01-30 13:52:49 +0100

    Tweak setup

--- setup.py
+++ setup.py
@@ -1,26 +1,29 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
-from wisdomhord import __version__
+
+try:
+    with open('README.rst', 'r', encoding='utf-8') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
 
 setup(
     name='wisdomhord',
-    version=__version__,
     author='Mika Naylor (Autophagy)',
     author_email='mail@autophagy.io',
-    url='https://github.com/Autophagy/wisdomhord',
     description='',
-    long_description=open('README.rst', encoding='utf-8').read(),
+    long_description=readme,
     packages=['wisdomhord'],
     install_requires=[
-        'datarum==0.2.0'
+        'datarum>=0.2.0'
     ],
-    python_requires='>=3',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
     ],
+    use_scm_version=True
 )
