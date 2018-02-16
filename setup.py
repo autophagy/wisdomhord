4454cdb61a1256836ee2b9a46256b6c70e52fe3f
Autophagy <mika@crate.io>
2018-02-16 15:20:39 +0100

    Prepare 0.1.0

--- setup.py
+++ setup.py
@@ -1,19 +1,32 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
-
-try:
-    with open('README.rst', 'r', encoding='utf-8') as f:
-        readme = f.read()
-except IOError:
-    readme = ''
+from wisdomhord import __version__
 
 setup(
     name='wisdomhord',
+    version=__version__,
     author='Mika Naylor (Autophagy)',
     author_email='mail@autophagy.io',
-    description='',
-    long_description=readme,
+    description='Flat file db tool',
+    long_description="""
+Wísdómhord is a flat file database tool for internal project use.
+
+The main goal of Wísdómhord is to create a flat file db format that produces
+files that are in line with my aesthetics, are easy to read and make sense to
+check into Github.
+
+.. image:: http://scieldas.autophagy.io/rtd/wisdomhord.png
+    :target: http://wisdomhord.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status
+
+.. image:: http://scieldas.autophagy.io/travis/Autophagy/wisdomhord.png
+    :target: https://travis-ci.org/Autophagy/wisdomhord
+    :alt: Build Status
+
+.. image:: http://scieldas.autophagy.io/licenses/MIT.png
+   :target: LICENSE
+   :alt: MIT License""",
     packages=['wisdomhord'],
     install_requires=[
         'datarum>=0.2.0'
@@ -24,6 +37,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
-    ],
-    use_scm_version=True
+    ]
 )
--- wisdomhord/__init__.py
+++ wisdomhord/__init__.py
@@ -3,4 +3,3 @@ from .bisen import Bisen, Sweor
 from .datatypes import String, Boolean, Integer, Float, DateTime
 
 __version__ = '0.1.0'
-__version_full__ = __version__
