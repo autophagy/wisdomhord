c79db717e549b91169f1b5a32e83c656d06d81a8
Autophagy <mika@crate.io>
2018-02-24 16:50:38 +0100

    Bump datarum version to 0.4

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ check into Github.
    :alt: MIT License""",
     packages=['wisdomhord'],
     install_requires=[
-        'datarum>=0.3.0'
+        'datarum==0.4.0'
     ],
     python_requires='>=3.6',
     classifiers=[
