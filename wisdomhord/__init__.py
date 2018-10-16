a649227c2daf8cf6f9bb524e215ef9338e4b9706
Autophagy <mika@crate.io>
2018-10-16 14:42:10 +0200

    Prepare version 0.3.0

--- wisdomhord/__init__.py
+++ wisdomhord/__init__.py
@@ -2,4 +2,4 @@ from .wisdomhord import hladan, cennan
 from .bisen import Bisen, Sweor
 from .datatypes import String, Boolean, Integer, Float, DateTime, Wending, Coordinate
 
-__version__ = "0.2.4"
+__version__ = "0.3.0"
