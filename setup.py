d09832c2c4a068a413fbf554377fe16ad562b026
Autophagy <mika@crate.io>
2018-10-17 22:11:30 +0200

    Bump datarum version to fix ARM bug

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ check into Github.
    :alt: MIT License""",
     packages=['wisdomhord'],
     install_requires=[
-        'datarum==0.5.0'
+        'datarum==0.5.1'
     ],
     python_requires='>=3.6',
     classifiers=[
